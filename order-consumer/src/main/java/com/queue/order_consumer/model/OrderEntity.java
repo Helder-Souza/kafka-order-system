184e39e88903a31781ad5e3579cd1c9543072027
Helder-Souza <helder825@hotmail.com>
2025-04-28 19:50:42 -0300

    add lombok

--- order-consumer/src/main/java/com/queue/order_consumer/model/OrderEntity.java
+++ order-consumer/src/main/java/com/queue/order_consumer/model/OrderEntity.java
@@ -2,10 +2,14 @@ package com.queue.order_consumer.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Data
 @Entity
+@NoArgsConstructor
 public class OrderEntity {
 
     @Id
@@ -13,28 +17,6 @@ public class OrderEntity {
     private String client;
     private String data;
     private final LocalDateTime creationDate = LocalDateTime.now();
+    private OrderStatus status;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getClient() {
-        return client;
-    }
-
-    public void setClient(String client) {
-        this.client = client;
-    }
-
-    public String getData() {
-        return data;
-    }
-
-    public void setData(String data) {
-        this.data = data;
-    }
 }
--- order-producer/src/main/java/com/queue/order_producer/model/Order.java
+++ order-producer/src/main/java/com/queue/order_producer/model/Order.java
@@ -1,32 +1,12 @@
 package com.queue.order_producer.model;
 
+import lombok.Data;
+
+@Data
 public class Order {
 
     private String id;
     private String client;
     private String data;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getClient() {
-        return client;
-    }
-
-    public void setClient(String client) {
-        this.client = client;
-    }
-
-    public String getData() {
-        return data;
-    }
-
-    public void setData(String data) {
-        this.data = data;
-    }
 }
