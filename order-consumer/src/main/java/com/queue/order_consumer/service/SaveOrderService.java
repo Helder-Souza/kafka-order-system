6ba95e012e816ed32c2f87454d3bf282d12038d4
Helder-Souza <helder825@hotmail.com>
2025-05-12 20:06:38 -0300

    handle events with status

--- order-consumer/src/main/java/com/queue/order_consumer/service/SaveOrderService.java
+++ order-consumer/src/main/java/com/queue/order_consumer/service/SaveOrderService.java
@@ -3,6 +3,7 @@ package com.queue.order_consumer.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.queue.order_consumer.model.OrderEntity;
+import com.queue.order_consumer.model.OrderStatus;
 import com.queue.order_consumer.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -13,13 +14,19 @@ public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
-    private ObjectMapper objectMapper = new ObjectMapper();
+    @Autowired
+    private ObjectMapper objectMapper;
 
     public void processOrder(String msg) {
         try {
-            orderRepository.save(objectMapper.readValue(msg, OrderEntity.class));
+            orderRepository.save(setStatusReceived(objectMapper.readValue(msg, OrderEntity.class)));
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private OrderEntity setStatusReceived(OrderEntity orderEntity) {
+        orderEntity.setStatus(OrderStatus.RECEIVED);
+        return orderEntity;
     }
 }
